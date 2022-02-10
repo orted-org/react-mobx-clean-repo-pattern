8981614891a9611ad0eff0cd2bfa7c39c7d41b3d
himanshu-sah <itsmehs07@gmail.com>
2022-02-10 10:58:16 +0545

    general layout for demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import NewTodo from "./Organs/NewTodo";
+import TodoList from "./Organs/TodoList";
+import TodoStatus from "./Organs/TodoStatus";
+import { StoresContext } from "./Logic/Providers/StoresProviders";
+import RTodo from "./Logic/Repository/RTodo";
+import { TodoStore } from "./Logic/State/TodoStore";
 
 function App() {
-  return <div></div>;
+  const todoRepo = new RTodo();
+  const todoStore = new TodoStore(todoRepo);
+  return (
+    <StoresContext.Provider value={{ todoStore }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <NewTodo />
+        <TodoStatus />
+        <TodoList />
+      </div>
+    </StoresContext.Provider>
+  );
 }
 
 export default App;
