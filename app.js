b8d43b595fb0f5da5369f12fac72b3f757aae972
himanshu-sah <itsmehs07@gmail.com>
2022-02-10 10:55:59 +0545

    added some delay in response

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomUUID } = require("crypto");
+const cors = require("cors");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 var TodoList = [];
 app.get("/", (req, res, next) => {
-  res.send({ data: TodoList });
+  const Timeout = setTimeout(() => {
+    res.send({ data: TodoList });
+    clearTimeout(Timeout);
+  }, 300);
 });
 
 app.post("/", (req, res, next) => {
-  const todo = req.body;
-  todo.id = randomUUID();
-  TodoList.push(todo);
-  res.send({ data: todo });
+  const Timeout = setTimeout(() => {
+    const todo = req.body;
+    todo.id = randomUUID();
+    TodoList.push(todo);
+    res.send({ data: todo });
+    clearTimeout(Timeout);
+  }, 300);
 });
 
 app.delete("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const newTodoList = [];
-  for (let i = 0; i < TodoList.length; i++) {
-    if (TodoList[i].id != id) {
-      newTodoList.push(TodoList[i]);
+  const Timeout = setTimeout(() => {
+    const id = req.params.id;
+    const newTodoList = [];
+    for (let i = 0; i < TodoList.length; i++) {
+      if (TodoList[i].id != id) {
+        newTodoList.push(TodoList[i]);
+      }
     }
-  }
-  TodoList = newTodoList;
-  res.send({ status: "ok" });
+    TodoList = newTodoList;
+    res.send({ status: "ok" });
+    clearTimeout(Timeout);
+  }, 300);
 });
 
 app.listen(4000, () => {
